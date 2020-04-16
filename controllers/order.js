e19a3cdf9352bc5f08ba5b3490f359de64a448c0
Nanda Gopal <ngopal253@gmail.com>
2020-04-16 22:31:07 +0530

    admin order tuples completed

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,16 @@
-const { Order, CartItem } = require('../models/order');
+const { Order } = require('../models/order');
 const { errorHandler } = require('../helpers/dbErrorHandler');
+
+exports.findOrderById = (req, res, next, id) => {
+  Order.findById(id)
+    .populate('products.product', 'name price')
+    .exec((err, order) => {
+      if (err || !order)
+        return res.status(400).json({ error: errorHandler(err) });
+      req.order = order;
+      next();
+    });
+};
 
 exports.create = (req, res) => {
   req.body.order.user = req.profile;
@@ -22,4 +33,15 @@ exports.listOrders = (req, res) => {
 
 exports.getStatusValues = (req, res) => {
   res.json(Order.schema.path('status').enumValues);
+};
+
+exports.updateOrderStatus = (req, res) => {
+  Order.update(
+    { _id: req.body.orderId },
+    { $set: { status: req.body.status } },
+    (err, order) => {
+      if (err) return res.status(400).json({ error: errorHandler(err) });
+      res.json(order);
+    }
+  );
 };
--- routes/order.js
+++ routes/order.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { findUserById, addOrderToUserHistory } = require('../controllers/user');
-const { create, listOrders, getStatusValues } = require('../controllers/order');
+const {
+  create,
+  listOrders,
+  getStatusValues,
+  findOrderById,
+  updateOrderStatus,
+} = require('../controllers/order');
 const { decreaseQuantity } = require('../controllers/product');
 
 router.post(
@@ -23,7 +29,15 @@ router.get(
   isAdmin,
   getStatusValues
 );
+router.put(
+  '/order/:orderId/status/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  updateOrderStatus
+);
 
 router.param('userId', findUserById);
+router.param('orderId', findOrderById);
 
 module.exports = router;
